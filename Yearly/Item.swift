35c2e3ee16addaab5e44df3db732fbf7074f4342
Dawson Knudtson <knudtdaw0000@gmail.com>
2025-08-20 17:35:26 -0500

    Initial Commit

---
+++ Yearly/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Yearly
+//
+//  Created by Dawson Knudtson on 8/20/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
